7c1687d9e79fb489ec140bbb50db5d299fa71f74
Robert Good <rgood@aacn.org>
2022-06-30 14:30:48 -0700

    Initial commit

---
+++ scripts/New-AzServicePrinciple.ps1
@@ -0,0 +1,50 @@
+#-----------------------------------------------------------------------
+# New-AzServicePrinciple [-TenantId [<Guid>]] [-SubscriptionId [<Guid>]]
+#
+# Example: .\Remove-LandingZone -TenantId -SubscriptionId -ResourceGroup -KeyVault -StorageAccount -Workspace
+# CLI: az ad sp create-for-rbac --name "myApp" --role contributor \
+#        --scopes /subscriptions/{subscription-id}/resourceGroups/{resource-group} --sdk-auth
+#-----------------------------------------------------------------------
+
+# ***
+# *** Parameters
+# ***
+param
+(
+    [string] $Name=$(throw '-Name is a required parameter. (myco-product-environment)'),
+    [string] $TenantId=$(throw '-TenantId is a required parameter. (00000000-0000-0000-0000-000000000000)'),
+    [string] $SubscriptionId=$(throw '-TenantId is a required parameter. (00000000-0000-0000-0000-000000000000)')
+)
+
+# ***
+# *** Initialize
+# ***
+if ($IsWindows) { Set-ExecutionPolicy Unrestricted -Scope Process -Force }
+$VerbosePreference = 'SilentlyContinue' #'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+[DateTime]$Now = Get-Date
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript on $Now"
+Write-Host "*****************************"
+# Imports
+Import-Module "./System.psm1"
+Install-Module -Name Az.Accounts -AllowClobber -Scope CurrentUser
+Install-Module -Name Az.Resources -AllowClobber -Scope CurrentUser
+
+# ***
+# *** Auth
+# ***
+Write-Host "*** Auth ***"
+Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId
+
+$sp = New-AzADServicePrincipal 
+$clientsec = [System.Net.NetworkCredential]::new("", $sp.Secret).Password
+$tenantID = (get-aztenant).Id
+$jsonresp = 
+    @{client_id=$sp.ApplicationId 
+        client_secret=$clientsec
+        tenant_id=$tenantID}
+    $jsonresp | ConvertTo-Json
+
